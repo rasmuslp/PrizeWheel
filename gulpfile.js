f12737ad80cefdb252ecc3dcbdda1d51d1dce278
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2015-04-17 14:26:12 +0200

    chore(build): now uses browsersync exclusively

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,9 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
-gulp.task('default', function() {
-  gulp.watch(['src/**/*.js'], [browserSync.reload]);
-  gulp.watch(['src/**/*.html'], [browserSync.reload]);
-  gulp.watch(['src/**/*.css'], [browserSync.reload]);
-  gulp.watch(['src/**/*.json'], [browserSync.reload]);
-
+gulp.task('watch', function() {
   browserSync({
+    files: 'src/*',
     server: {
       baseDir: 'src/'
     }
