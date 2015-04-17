7988628c7014c7aaaab534b25cb371937bf78893
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2015-04-17 17:21:14 +0200

    feat(Canvas): wheel and arrow is drawn
    TODO: Rotate and text

--- src/wheel.js
+++ src/wheel.js
@@ -1,52 +1,99 @@
 (function() {
   'use strict';
 
+  var slices = [];
+  var numberOfSlices;
+  var sliceArc;
+  var currentAngle = 0;
+
+  var draw = function() {
+    var canvas = document.getElementById('wheel');
+    if (typeof canvas.getContext === 'undefined') {
+      console.error('Canvas not supported by this browser');
+    }
+
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.lineWidth = 2;
+
+    var arrowHeight = 30;
+    var size = canvas.width;
+
+    var outsideRadius = (size-arrowHeight)/2 - 5;
+    var insideRadius = 20;
+
+    var cX = size/2;
+    var cY = outsideRadius + arrowHeight + 2;
+
+    var i;
+    var angle;
+
+    // Slice fill
+    for (i = 0; i < numberOfSlices; i++) {
+      angle = currentAngle + i * sliceArc;
+      ctx.beginPath();
+      ctx.arc(cX, cY, outsideRadius, angle, angle + sliceArc, false);
+      ctx.arc(cX, cY, insideRadius, angle + sliceArc, angle, true);
+
+      ctx.fillStyle = slices[i].color;
+      ctx.fill();
+    }
+
+    // Slice outlines
+    for (i = 0; i < numberOfSlices; i++) {
+      angle = currentAngle + i * sliceArc;
+      ctx.fillStyle = 'black';
+      ctx.beginPath();
+      ctx.arc(cX, cY, outsideRadius, angle, angle + sliceArc, false);
+      ctx.arc(cX, cY, insideRadius, angle + sliceArc, angle, true);
+      ctx.stroke();
+    }
+
+    // Arrow
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.moveTo(cX - 5, 0);
+    ctx.lineTo(cX + 5, 0);
+    ctx.lineTo(cX + 5, 20);
+    ctx.lineTo(cX + 10, 20);
+    ctx.lineTo(cX, 30);
+    ctx.lineTo(cX - 10, 20);
+    ctx.lineTo(cX - 5, 20);
+    ctx.lineTo(cX - 5, 0);
+    ctx.fill();
+  };
+
   $(document).ready(function() {
+    var i;
 
     /* jshint -W117 */
-    var sliceData = data;
+    if (typeof data === 'undefined') {
+      console.error('No data found :<');
+      //TODO: Write 'no data' to screen
+      return;
+    }
+
+    console.log('Got data: %o', data);
+
+    for (i = 0; i < data.length; i++) {
+      slices.push({
+        text: data[i]
+      });
+    }
     /* jshint +W117 */
 
-    console.log(sliceData);
+    numberOfSlices = slices.length;
+    sliceArc = Math.PI * 2 / numberOfSlices;
 
-    var numberOfBars = 360;
-    var numberOfSlices = sliceData.length;
-
-    var wheel = $('#wheel');
-    var colors = [];
-    var colorSeparation = numberOfBars / numberOfSlices;
-
-    for (var i = 0; i < numberOfSlices; i++) {
-      colors.push( 'hsl(' + i * ( colorSeparation ) + ', 100%, 50%)' );
+    var colorSeparation = 360 / numberOfSlices;
+    for (i = 0; i < numberOfSlices; i++) {
+      slices[i].color = 'hsl(' + i * ( colorSeparation ) + ', 100%, 50%)';
     }
 
-    var currentColor = -1;
-    var bars = [];
-    for (i = 0; i < numberOfBars; i++) {
-      if ( i % colorSeparation === 0) {
-        // New slice
-        currentColor++;
-      }
+    console.log('Data is now: %o', slices);
 
-      var bar = $('<div></div>');
-      bar.css('background-color', colors[ currentColor ] );
-      bar.addClass('bar');
-      bar.css('-webkit-transform', 'rotate(' + i + 'deg)');
-      wheel.append(bar);
-      bars.push( bar );
-    }
-
-    var angle = 0;
-
-    var rotate = function() {
-      angle += 2;
-      for (var i = 0; i < numberOfBars; i++) {
-        bars[i].css('-webkit-transform', 'rotate(' + ( angle + i ) + 'deg)');
-      }
-    };
-
-    setInterval( rotate, 1000 / 60 );
-
+    draw();
   });
 
 })();
