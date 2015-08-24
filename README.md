7db2bf9cb3e680c94ff3077fc514c21a9fff1a22
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2015-08-24 21:34:34 +0200

    chore(): ready for release

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PrizeWheel
+
+Press the space button to start and stop the wheel.
+
+See it in action here: [http://rasmuslp.github.io/PrizeWheel/](http://rasmuslp.github.io/PrizeWheel/).
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 'use strict';
 
 var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
 var browserSync = require('browser-sync');
+
+gulp.task('deploy', function () {
+  return gulp.src('src/**/*')
+  .pipe(ghPages());
+});
 
 gulp.task('watch', function() {
   browserSync({
